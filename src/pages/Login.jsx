b59f3bf650f4f1191895e382b5631d5030cf8cc5
SnappiBrawn <creativeswaroop@gmail.com>
2023-08-29 21:02:04 +0530

    added login and sign up pages

---
+++ src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../images/logo.JPG";
+
+const Login = () =>{
+    return (
+    <div className="formContainer">
+        <div className="formWrapper">
+        <img className="logo" src={logo} alt="WebTextsLogo"/>
+        <span className="info"><b>Login</b></span>
+            <form>
+                <input type="email" placeholder="Email ID" />
+                <input type="password" placeholder="Password" />
+                <button>Login</button>
+            </form>
+            <p>Don't have an account yet? Sign Up</p>
+        </div>
+    </div>
+    );
+}
+
+export default Login;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "../images/logo.JPG";
+import placeholder from "../images/profile_upload_placeholder.png";
+
+const Register = () =>{
+    return (
+    <div className="formContainer">
+        <div className="formWrapper">
+        <img className="logo" src={logo} alt="WebTextsLogo"/>
+        <span className="info"><b>Register</b></span>
+            <form>
+                <input type="text" placeholder="Username" />
+                <input type="email" placeholder="Email ID" />
+                <input type="password" placeholder="Password" />
+                <input type="file" id="profilePic" hidden/>
+                <label htmlFor="profilePic"><img height="30" src={placeholder}/><span>Pick an Avatar</span></label>
+                <button>Sign Up</button>
+            </form>
+            <p>Do you already have an account? Sign In</p>
+        </div>
+    </div>
+    );
+}
+
+export default Register;

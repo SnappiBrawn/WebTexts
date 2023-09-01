318aa344874c3f30faa4444ac9572c28cf469a4b
SnappiBrawn <creativeswaroop@gmail.com>
2023-09-02 01:56:24 +0530

    implemented search with convo init protocols

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,99 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import { db } from "../firebase";
+import { collection, query, where ,doc , getDocs, getDoc, setDoc, updateDoc, serverTimestamp } from "firebase/firestore";
+import { AuthContext } from "../context/AuthContext";
 
 const Search = () => {
+  const [searchName,setSearchName] = useState("");
+  const [user,setUser] = useState(null);
+  const [err,setErr] = useState(false);
+
+  const {currentUser} = useContext(AuthContext)
+
+  const userSearch = async ()=>{
+    const q = query(collection(db, "users"), where("displayName", "==", searchName));
+    try{
+      const querySnapshot = await getDocs(q);
+      if(querySnapshot.docs.length==0){
+        setErr(true);
+        setUser(null);
+      } 
+      querySnapshot.forEach((doc) => {
+        setErr(false);
+        setUser(doc.data());
+      });
+    }
+    catch(error){
+      console.log(error);
+      setErr(true);
+    }
+  }
+
+  const clearSearch = () =>{
+    setErr(false);
+    setUser(null);
+    setSearchName("")
+    document.getElementById("search-field").value="";
+  }
+
+  const triggerSearch = (e) =>{
+    if(e.code === "Enter"){
+      if(searchName==""){
+        setErr(false);
+        setUser(null);
+      }
+      else{
+        userSearch();
+      }
+    }
+  }
+
+  const addChat = async () =>{
+    const chatId = currentUser.uid > user.uid ? currentUser.uid+user.uid : user.uid+currentUser.uid
+    try {
+      const res = await getDoc(doc(db, "chats", chatId));
+      if(!res.exists()){
+        await setDoc(doc(db, "chats", chatId),{messages:[]})
+        await updateDoc(doc(db, "userChats",currentUser.uid), {
+          [chatId+".userInfo"]:{
+            uid:user.uid,
+            displayName:user.displayName,
+            photoURL:user.photoURL
+          },
+          [chatId+".date"]: serverTimestamp()
+        });
+        await updateDoc(doc(db, "userChats",user.uid), {
+          [chatId+".userInfo"]:{
+            uid:currentUser.uid,
+            displayName:currentUser.displayName,
+            photoURL:currentUser.photoURL
+          },
+          [chatId+".date"]: serverTimestamp()
+        });
+      }
+      clearSearch();
+    } 
+    catch (error) {
+      setErr(true);
+      console.log(error);
+    }
+    
+  }
+
   return (
     <div className='search'>
       <div className="searchForm">
         <span className="fas fa-search"></span>
-        <input type="text" placeholder='Search for a user'></input>
+        <input id="search-field" type="text" placeholder='Search for a user' onKeyDown={triggerSearch} onChange={(e)=>{setSearchName(e.target.value)}}></input>
+        <span className="fas fa-times" onClick={clearSearch}></span>
       </div>
-      
-      <div className="userChat">
-        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.goodfreephotos.com%2Fcache%2Fpeople%2Fbeautiful-woman-with-blonde-hair-with-green-eyes_w80_h80_cw80_ch80_thumb.jpg%3Fcached%3D1524708655&f=1&nofb=1&ipt=7854461e7b9c82ac3797efd74673cd5a75da4c188520958b37a03f80b559bed7&ipo=images"/>
+      {err && <span>No such user found!</span>}
+      {user && <div className="userChat" onClick={addChat}>
+        <img src={user.photoURL}/>
         <div className="userChatInfo">
-          <span>Eva</span>
+          <span>{user.displayName}</span>
         </div>
-      </div>
+      </div>}
       
     </div>
   )
